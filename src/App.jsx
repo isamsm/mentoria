61b2329c4323235c3a469559957793ad264683c3
isamsm <isamarsques@gmail.com>
2024-05-18 23:37:46 -0300

    feat: anotações

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import calendar from './assets/calendar.svg'
 import company from './assets/company.svg'
 import file from './assets/file.svg'
 import download from './assets/download.svg'
+import calendar_2 from './assets/calendar_2.svg'
 
 function App() {
   return (
@@ -26,10 +27,10 @@ function App() {
           <Header />
           <div className="bg-slate-100 text-black">
             <div className="px-2 md:px-10 lg:px-10 py-5">
-              <div className="flex flex-col md:flex-row lg:flex-row justify-between pb-5">
+              <div className="flex md:justify-between lg:justify-between gap-5 md:gap-0 lg:gap-0 pb-5">
                 <h3 className="text-xl font-bold"> Mentorados</h3>
-                <div className="flex gap-5">
-                  <img src={arrow} className="h-5 w-5" />
+                <div className="flex gap-4 items-center">
+                  <img src={arrow} className="h-4 w-4" />
                   <p> Voltar</p>
                 </div>
               </div>
@@ -211,8 +212,8 @@ function App() {
                   </div>
                 </div>
               </div>
-              <div className="flex flex-col lg:flex-row pt-5 gap-3">
-                <div className="bg-white border-2 border-slate-200 rounded-lg w-72 lg:w-6/12 lg:h-96 p-5">
+              <div className="flex flex-col md:flex-row lg:flex-row pt-5 gap-3">
+                <div className="bg-white border-2 border-slate-200 rounded-lg w-72 lg:w-6/12 p-5">
                   <div className="flex gap-5 justify-between md:items-center lg:items-center">
                     <h3 className="text-2xl font-bold"> Anexos </h3>
                     <img src={plus} className="h-10 w-10" />
@@ -225,7 +226,7 @@ function App() {
                         </div>
                         <div>
                           <div className="flex flex-col lg:flex-row justify-between">
-                            <h2 className="font-bold pr-44">
+                            <h2 className="font-bold pr-40">
                               {' '}
                               ATA DE REUNÃO 05.12.23 - Italo Silva.pdf{' '}
                             </h2>
@@ -250,7 +251,7 @@ function App() {
                         </div>
                         <div>
                           <div className="flex flex-col lg:flex-row justify-between">
-                            <h2 className="font-bold pr-44">
+                            <h2 className="font-bold pr-40">
                               {' '}
                               ATA DE REUNÃO 05.12.23 - Italo Silva.pdf{' '}
                             </h2>
@@ -268,19 +269,91 @@ function App() {
                     </div>
                   </div>
                 </div>
-                <div className="bg-white border-2 border-slate-200 rounded-lg w-72 lg:w-6/12 h-96 p-5">
-                  <div className="flex flex-col md:flex-row lg:flex-row gap-5 justify-between md:items-center lg:items-center">
+                <div className="bg-white overflow-hidden border-2 border-slate-200 rounded-lg w-72 lg:w-6/12 p-5 font-poppins h-full crop">
+                  <div className="flex gap-5 justify-between md:items-center lg:items-center">
                     <h3 className="text-2xl font-bold"> Anotações </h3>
                     <img src={plus} className="h-10 w-10" />
                   </div>
                   <div className="flex flex-col lg:flex-row pt-5">
-                    <div className="bg-white border-2 border-slate-200 lg:w-full rounded-lg p-5">
-                      <div className="flex flex-col lg:flex-row gap-5 lg:items-center"></div>
+                    <div className="border-2 border-slate-200 lg:w-full rounded-lg p-5">
+                      <div className="flex flex-col lg:flex-row gap-5 lg:items-center">
+                        <div>
+                          <img src={italo} className="h-9 w-9 rounded-full" />
+                        </div>
+                        <div>
+                          <div className="flex justify-between items-center">
+                            <h2 className="font-bold"> Helio Martins </h2>
+                            <div className="flex gap-2 lg:pl-80">
+                              <img src={calendar_2} className="h-5 w-5" />
+                              <p className="text-sm"> 25/10/24 </p>
+                            </div>
+                          </div>
+                          <div className="flex flex-col gap-5">
+                            <p className="text-sm text-slate-500">
+                              {' '}
+                              Mentor educação{' '}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex pt-3 justify-between pb-5">
+                        <h3 className="text-lg font-bold">
+                          {' '}
+                          Pontos de melhorias em sua timidez{' '}
+                        </h3>
+                        <img src={trash} className="h-5 w-5" />
+                      </div>
+                      <p className="text-sm text-slate-500">
+                        {' '}
+                        Lorem ipsum dolor sit amet consectetur. Viverra tempus
+                        vitae donec non ultrices. Habitant amet in gravida ante
+                        proin. Adipiscing ut et enim in. Vitae lacus pharetra
+                        pellentesque sodales eget porta euismod sit. Tempor
+                        aliquet turpis sagittis sed quisque. Ultricies tempus
+                        facilisi mattis egestas sit tellus donec et sapien.
+                        Elementum rhoncus{' '}
+                      </p>
                     </div>
                   </div>
                   <div className="flex flex-col lg:flex-row pt-5">
-                    <div className="bg-white border-2 border-slate-200 lg:w-full rounded-lg p-5">
-                      <div className="flex flex-col lg:flex-row gap-5 lg:items-center"></div>
+                    <div className="border-2 border-slate-200 lg:w-full rounded-lg p-5">
+                      <div className="flex flex-col lg:flex-row gap-5 lg:items-center">
+                        <div>
+                          <img src={italo} className="h-9 w-9 rounded-full" />
+                        </div>
+                        <div>
+                          <div className="flex justify-between items-center">
+                            <h2 className="font-bold"> Helio Martins </h2>
+                            <div className="flex gap-2 lg:pl-80">
+                              <img src={calendar_2} className="h-5 w-5" />
+                              <p className="text-sm"> 25/10/24 </p>
+                            </div>
+                          </div>
+                          <div className="flex flex-col gap-5">
+                            <p className="text-sm text-slate-500">
+                              {' '}
+                              Mentor educação{' '}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex pt-3 justify-between pb-5">
+                        <h3 className="text-lg font-bold">
+                          {' '}
+                          Pontos de melhorias em sua timidez{' '}
+                        </h3>
+                        <img src={trash} className="h-5 w-5" />
+                      </div>
+                      <p className="text-sm text-slate-500">
+                        {' '}
+                        Lorem ipsum dolor sit amet consectetur. Viverra tempus
+                        vitae donec non ultrices. Habitant amet in gravida ante
+                        proin. Adipiscing ut et enim in. Vitae lacus pharetra
+                        pellentesque sodales eget porta euismod sit. Tempor
+                        aliquet turpis sagittis sed quisque. Ultricies tempus
+                        facilisi mattis egestas sit tellus donec et sapien.
+                        Elementum rhoncus{' '}
+                      </p>
                     </div>
                   </div>
                 </div>
