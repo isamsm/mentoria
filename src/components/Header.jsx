fb85e87c31099b0f1075345ab923498d99ee5f68
isamsm <isamarsques@gmail.com>
2024-06-21 10:52:50 -0300

    fix: melhorias mobile, principalmente header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,20 +6,18 @@ import arrow from '../assets/header/arrow.svg'
 export default function Header() {
     return (
         <>
-            <nav className='flex w-screen fixed flex-row bg-white gap-0 md:justify-between lg:justify-between items-center h-24 lg:h-20 text-black'>
+            <nav className='flex w-9/12 md:w-screen lg:w-screen fixed flex-row bg-white gap-0 justify-between items-center h-20 text-black'>
                 <div className='flex flew-row gap-1 md:gap-5 lg:gap-5 mx-5 items-center'>
                     <img src={mentoring} className='h-8 w-8 md:h-12 lg:h-12 md:w-12 lg:w-12 rounded' alt='Icone mentoria'/>
                     <p className='font-semibold pt-2'> Mentoria </p>
-                    <img src={arrow} className='h-2.4' alt='Icone de seta'/>
+                    <img src={arrow} className='h-2.4 pt-2' alt='Icone de seta'/>
                 </div>
-                <div className='flex flex-col md:flex-row lg:flex-row gap-1 md:gap-5 lg:gap-5 mx-1 md:mx-32 lg:mx-32'>
-                    <div className='flex flex-row gap-3'>
-                        <img src={notifications} className='h-8 md:h-12 lg:h-12' alt='Icone notificações'/>
-                        <img src={yara} className='h-8 w-8 md:h-12 md:w-12 lg:h-12 lg:w-12 rounded' alt='Foto de perfil do usuário'/>
-                    </div>
-                    <div className='flex flex-row md:flex-col lg:flex-col gap-1 md:gap-0 lg:gap-0'>
+                <div className='flex flex-row gap-1 md:gap-5 lg:gap-5 mx-0 md:mx-32 lg:mx-32'>
+                    <img src={notifications} className='h-8 md:h-12 lg:h-12' alt='Icone notificações'/>
+                    <img src={yara} className='h-8 w-8 md:h-12 md:w-12 lg:h-12 lg:w-12 rounded' alt='Foto de perfil do usuário'/>
+                    <div className='flex flex-row md:flex-col lg:flex-col gap-1 md:gap-0 lg:gap-0 items-center md:items-start lg:items-start'>
                         <p className='font-semibold'> Yara </p>
-                        <p className='font-inter text-slate-500'> yara@gmail.com </p>
+                        <p className='font-inter text-slate-500 hidden md:block lg:block'> yara@gmail.com </p>
                     </div>
                 </div>
             </nav>
